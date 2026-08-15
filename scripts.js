fd569a30bb09fdfeab27025ed1522b989752250f
Sam Taheri <samtaheri1000@gmail.com>
2026-08-15 16:15:08 -0400

    improved navbar design and funcitonality across all webpages and devices as well as js behaviors, page paddings and margins, and added contact information

--- scripts.js
+++ scripts.js
@@ -108,6 +108,7 @@ testimonyItems.forEach(function(item) {
 const menuButton = document.querySelector(".menu-button");
 const navLinks = document.querySelector(".nav-links");
 
+
 menuButton.addEventListener("click", function(event) {
     event.stopPropagation();
 
@@ -123,3 +124,23 @@ document.addEventListener("click", function() {
     navLinks.classList.remove("show-menu");
     menuButton.classList.remove("rotate");
 });
+
+//JS for dropdown menu actions
+
+const classesLink = document.querySelector(".dropdown > a");
+const dropdownMenu = document.querySelector(".dropdown-menu");
+
+classesLink.addEventListener("click", function(event) {
+    event.preventDefault();
+    event.stopPropagation();
+
+    
+    dropdownMenu.classList.toggle("show-dropdown");
+});
+
+
+document.addEventListener("click", function(event) {
+    if (!event.target.closest(".dropdown")) {
+        dropdownMenu.classList.remove("show-dropdown");
+    }
+}, true);
